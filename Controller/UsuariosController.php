cadfedfb29f0f016621fb6daeec4da472d99ada1
LuciMadri <89278285+LuciMadri@users.noreply.github.com>
2022-10-07 18:32:37 -0600

    + semana 04
    jquery, modelo y controller

--- Controller/UsuariosController.php
+++ Controller/UsuariosController.php
@@ -1,5 +1,6 @@
 <?php
 
+// Si alguien hizo un post a ese btn 
 if(isset($_POST["btnIngresar"]))
 {
     //Crear la base de datos, crear la tabla, crear el procedimiento
--- View/js/login.js
+++ View/js/login.js
@@ -1,32 +1,45 @@
-$(document).ready(function() {
-    HabilitarBoton(true);
+// Vamos a consumir un API
+// Hay dos formas, una con JS y otra con JQuery
+// Ajax permite consumir un API pero con JQuery
+$(document).ready(function () {
+  HabilitarBoton(true);
 });
 
 function ValidarDatos() {
-    let identificacion = $("#txtIdentificacion").val().trim();
 
-    if (identificacion != "") {
+  // Trim elimina los espacion en blanco/vacios
+  // El # es para las variables
+  let identificacion = $("#txtIdentificacion").val().trim();
 
-        $.ajax({
-            type: "GET",
-            url: 'https://apis.gometa.org/cedulas/' + identificacion,
-            data: { },
-            datatype: 'json',
-            success: function(response)
-            {
-                if (response.resultcount == undefined || response.resultcount == 0) {
-                    HabilitarBoton(true);
-                }
-                else {
-                    HabilitarBoton(false);
-                }
-            }
-       });
+  if (identificacion != "") {
 
-    }
+    // Estos es una Ajax
+    $.ajax({
+      // Optenemos datos con get
+      type: "GET",
+      // Aqui ponemos la ruta de donde vamos a obtener esos datos
+      // Obtenemos info de la variable
+      url: "https://apis.gometa.org/cedulas/" + identificacion,
+
+      // Data sirve para pasar datos en un body
+      data: {},
+
+      // Tipo de la respuesta del llamado 
+      datatype: "json",
+
+      success: function (response) {
+
+        //Si no existe desahibilitar el btn ingresar
+        if (response.resultcount == undefined || response.resultcount == 0) {
+          HabilitarBoton(true);
+        } else {
+          HabilitarBoton(false);
+        }
+      },
+    });
+  }
 }
 
-function HabilitarBoton(flag)
-{
-    $("#btnIngresar").prop("disabled", flag);
+function HabilitarBoton(flag) {
+  $("#btnIngresar").prop("disabled", flag);
 }
